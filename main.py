4fc6dc167c468ddd438c013095a7e9bbb2207525
ownerairakitchen <triaramadanti@student.upi.edu>
2026-03-30 18:37:50 +0700

    Gua si C

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ def main () :
     a = 20
     b = 10
     print(a-b)
+    # fungsi pembagian
+    a = 20
+    b = 10
+    print(a/b)
+    # FUngsi Pertambahan
+    a = 10
+    b = 11
+    print(a+b)
 
 main
